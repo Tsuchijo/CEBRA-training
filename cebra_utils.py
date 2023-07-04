1e5e0d22dc02e7ac31fb8db9a42fc685a8d98f59
murph_4090ws <TsuchitoriJoel@gmail.com>
2023-07-04 11:44:18 -0700

    added linear classifier and script to help label

---
+++ cebra_utils.py
@@ -0,0 +1,121 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import cebra
+from PIL import Image
+import cv2
+import os
+import torch
+import torch.nn.functional as F
+import itertools
+import random
+from torch import nn
+import cebra.models
+import cebra.data
+from cebra.models.model import _OffsetModel, ConvolutionalModelMixin
+
+class ChangeOrderLayer(nn.Module):
+    def forward(self, x):
+        return x.movedim(-2,1)  # Permute dimensions 1 and 2
+
+@cebra.models.register("convolutional-model-offset11")
+class ConvulotionalModel1(_OffsetModel, ConvolutionalModelMixin):
+
+    def __init__(self, num_neurons, num_units, num_output, normalize=True):
+        super().__init__(
+            ## create a model which goes from a 128 x 128 image to a 1d vector
+            ## of length num_output
+            ChangeOrderLayer(),
+            nn.Conv2d(5, 16, kernel_size=3, stride=1, padding=1),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size=4, stride=4),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size=4, stride=4),
+            nn.Flatten(),
+            nn.Linear(1024, num_output),
+
+            num_input=num_neurons,
+            num_output=num_output,
+            normalize=normalize,
+        )
+
+    # ... and you can also redefine the forward method,
+    # as you would for a typical pytorch model
+
+    def get_offset(self):
+        return cebra.data.Offset(2, 3)
+
+def process_brain(brain_seq):
+    brain_seq = np.array(brain_seq)
+    flat_seq = np.array([(brain_frame.flatten()) for brain_frame in brain_seq])
+    return flat_seq.astype(float)
+
+## Loads data from a folder of TIF files
+# filepath: path to folder
+# processor: function to process each image
+# max: max images to load as a proportion of array size
+# min: min images to load as a proportion of array size
+# returns: list of processed images, list of filenames
+def import_data(filepath, processor, min = 0, max = 1):
+    output_data = []
+    output_name = []
+    path_list = os.listdir(filepath)
+    path_list.sort()
+    random.Random(4).shuffle(path_list)
+    min_index = int(min * len(path_list))
+    max_index = int(max * len(path_list))
+    for file in itertools.islice(path_list, min_index, max_index):
+     filename = os.fsdecode(file)
+     if filename.endswith(".tif"):
+         out = cv2.imreadmulti(filepath + '/' + filename)[1]
+         output_data.append(processor(out))
+         output_name.append(filename.split('.')[0])
+     elif filename.endswith(".npy"):
+         output_data.append(processor(np.load(filepath + '/' + filename)))
+         output_name.append(filename.split('.')[0])
+     else:
+         continue
+    return output_data, output_name
+
+def flatten_data(data):
+    return np.concatenate(data, axis=0)
+
+def pad_data(data, pre, post):
+    pre_padding = np.repeat(np.expand_dims(data[0], axis = 0), pre, axis=0)
+    post_padding = np.repeat(np.expand_dims(data[-1], axis = 0), post, axis=0)
+    return np.concatenate((pre_padding, data, post_padding), axis=0)
+
+def generate_CEBRA_embeddings(model, data, session_id, offset = (2,3)):
+    data_torch = torch.empty(0,5,128,128)
+    padded = pad_data(data, offset[0], offset[1])
+    for i, frame in enumerate(data):
+        frame = torch.from_numpy(np.array(padded[i: i + offset[0] + offset[1]])).float().unsqueeze(0)
+        data_torch = torch.cat((data_torch, frame), dim = 0)
+    data_torch = data_torch.swapdims(-2, 1)
+    embedding = model[session_id](data_torch).detach().cpu().numpy().squeeze()
+    return embedding
+
+def load_model(model_path):
+    #find available device
+    saved_solver = torch.load(model_path)
+    model = saved_solver.model
+    return model
+
+def reshape_frames(frames, shape_ref):
+    shape_list = [np.shape(x)[0] for x in shape_ref]
+    gen_video_list = []
+    index = 0
+    for shape in shape_list:
+        gen_video_list.append((frames[index : index + shape]))
+        index += shape
+    return gen_video_list
+
+#choose a random window of set size from the data deterministically based on seed
+def choose_random_window( window_size, seed, data):
+    random.seed(seed)
+    start = random.randint(0, len(data) - window_size)
+    return data[start:start+window_size]
+
+def normalize_array(in_array):
+    return np.array([x / np.linalg.norm(x) for x in in_array])
--- label_videos.py
+++ label_videos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/home/murph_4090ws/miniconda3/envs/cebra-env/bin/python
 import sys
 import numpy as np
 from PIL import Image
@@ -24,7 +24,6 @@ def import_data(filepath, processor, min = 0, max = 1):
     for file in itertools.islice(path_list, min_index, max_index):
      filename = os.fsdecode(file)
      if filename.endswith(".tif"):
-         print(filename)
          out = cv2.imreadmulti(filepath + '/' + filename)[1]
          output_data.append(processor(out))
          output_name.append(filename.split('.')[0])
@@ -39,50 +38,55 @@ data_directory = '/mnt/teams/Tsuchitori/MV1_run_30hz_30frame_brain2behav_DFF_new
 behavior_data_paths = [  data_directory + 'camera1/' + \
                      file for file in os.listdir(data_directory + 'brain/')]
 
-data_path = behavior_data_paths[0]
-
-# Load data
-behavior_data, behavior_names = import_data(data_path, lambda x : x, 0, 1)
-labels = dict()
-print('running')
-
 key_behavior = {
     0: 'still',
     1: 'sniffing',
     2: 'walking',
-    3: 'chewing',
+    3: 'grooming',
 }
 
-for video, name in zip(behavior_data, behavior_names):
-    # Open the video file
-    if name.split('_')[0] == 'nomove':
-        labels[name] = 'still'
-        continue
-    while True:
-        exit = False
-        for frame in video:
-            # Display the frame to the user
-            cv2.imshow('Video', frame)
-            key = cv2.waitKey(12) & 0xFF # Wait 12ms for user input
-            # Exit the loop and close the window when 'q' is pressed
-            if key == ord('q'):
-                exit = True
+for data_path in behavior_data_paths:
+    # Load data
+    behavior_data, behavior_names = import_data(data_path, lambda x : x, 0, 0.2)
+    labels = dict()
+    print('labelling: ' + str(len(behavior_data)) + ' videos')
+
+    # create one hot encoding dictionary for each behavior
+    one_hot = dict()
+    for i, behavior in enumerate(key_behavior.values()):
+        one_hot[behavior] = np.zeros(len(key_behavior))
+        one_hot[behavior][i] = 1
+
+    for video, name in zip(behavior_data, behavior_names):
+        # Open the video file
+        if name.split('_')[0] == 'nomove':
+            labels[name] = one_hot['still']
+            continue
+        while True:
+            exit = False
+            for frame in video:
+                # Display the frame to the user
+                cv2.imshow('Video', frame)
+                key = cv2.waitKey(12) & 0xFF # Wait 12ms for user input
+                # Exit the loop and close the window when 'q' is pressed
+                if key == ord('q'):
+                    exit = True
+                    break
+                # if a number is pressed save that number to the labels
+                elif key >= ord('0') and key <= ord('9'):
+                    labels[name] =  one_hot[key_behavior[key - ord('0')]]
+                    exit = True
+                    break
+
+            # Restart the video when the loop ends
+            if exit:
                 break
-            # if a number is pressed save that number to the labels
-            elif key >= ord('0') and key <= ord('9'):
-                labels[name] =  key_behavior[key - ord('0')]
-                exit = True
-                break
-
-        # Restart the video when the loop ends
-        if exit:
-            break
 
 
-    cv2.destroyAllWindows()
-# Save the labels to a pickle file
-import pickle
-file = data_path.split('/')[-1]
-with open(file + 'labels.pickle', 'wb') as handle:
-    pickle.dump(labels, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        cv2.destroyAllWindows()
+    # Save the labels to a pickle file
+    import pickle
+    file = data_path.split('/')[-1]
+    with open(file + 'labels.pickle', 'wb') as handle:
+        pickle.dump(labels, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
