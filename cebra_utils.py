9c4016b39c6b4cb9bd03e9dab8181b0e1d4fd6f1
murph_4090ws <TsuchitoriJoel@gmail.com>
2023-08-16 16:45:01 -0700

    updated code to work with similar architecture to mesogan

--- cebra_utils.py
+++ cebra_utils.py
@@ -81,8 +81,132 @@ class ConvulotionalModel30Frame(_OffsetModel, ConvolutionalModelMixin):
     # ... and you can also redefine the forward method,
     # as you would for a typical pytorch model
 
+
+@cebra.models.register("Mesogan-Encoder")
+class AttnLSTMEncoder(_OffsetModel, ConvolutionalModelMixin):
+    def __init__(self, num_neurons, num_units, num_output, normalize=True, ch=64, lstm_hidden=512, attn_kernel=3):
+        super().__init__(
+            num_input=num_neurons,
+            num_output=num_output,
+            normalize=normalize,
+        )
+
+        self.downsample = nn.Sequential(
+            # (1, 128, 128) -> (64, 128, 128)
+            nn.Conv2d(1, ch, kernel_size=7, stride=1, padding=3, bias=False),
+            nn.BatchNorm2d(ch),
+            nn.ReLU(inplace=True),
+            # (64, 128, 128) -> (128, 64, 64)
+            nn.Conv2d(ch, ch * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ch * 2),
+            nn.ReLU(inplace=True),
+            # (128, 64, 64) -> (256, 32, 32)
+            nn.Conv2d(ch * 2, ch * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ch * 4),
+            nn.ReLU(inplace=True),
+            # (256, 32, 32) -> (512, 16, 16)
+            nn.Conv2d(ch * 4, ch * 8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ch * 8),
+            nn.ReLU(inplace=True),
+            # (512, 16, 16) -> (512, 8, 8)
+            nn.Conv2d(ch * 8, ch * 8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ch * 8),
+            nn.ReLU(inplace=True),
+        )
+
+        if attn_kernel == 1: padding = 0
+        elif attn_kernel == 3: padding = 1
+
+        self.attention = nn.Conv2d(ch * 8 + lstm_hidden, 1, kernel_size=attn_kernel, stride=1, padding=padding, bias=False)
+
+        self.net_h0 = nn.Sequential(
+            # (512, 8, 8) -> (lstm_hidden, 4, 4)
+            nn.Conv2d(ch * 8, lstm_hidden, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(lstm_hidden),
+            nn.ReLU(inplace=True),
+            # (512, 4, 4) -> (512, 1, 1)
+            nn.AvgPool2d(4)
+        )
+
+        self.net_c0 = nn.Sequential(
+            # (512, 8, 8) -> (512, 4, 4)
+            nn.Conv2d(ch * 8, lstm_hidden, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(lstm_hidden),
+            nn.ReLU(inplace=True),
+            # (512, 4, 4) -> (512, 1, 1)
+            nn.AvgPool2d(4)
+        )
+        self.lstm = nn.LSTM(input_size=ch * 8, hidden_size=lstm_hidden, batch_first=True)
+
+    def forward(self, x):
+        # swap dimensions 1 and 2
+        x = x.movedim(1, 2)
+        x = x[:, :, None, : , :]
+        n, t = x.size(0), x.size(1)
+
+        # x: (n * t, 1, 128, 128) -> (n * t, 512, 8, 8)
+        x = x.view(n * t, x.size(2), x.size(3), x.size(4))
+        h = self.downsample(x)
+
+        # (n * t, 512, 8, 8) -> (n, t, 512, 8, 8)
+        h_expand = h.view(n, t, h.size(1), h.size(2), h.size(3))
+        # (n, t, 512, 8, 8) -> (n, 512, 8, 8)
+        h_mean = torch.mean(h_expand, dim=1)
+
+        # (n, 512, 8, 8) -> (n, 512, 1, 1)
+        h0 = self.net_h0(h_mean)
+        c0 = self.net_c0(h_mean)
+
+        # at each time-step, compute an attention map
+        attn_maps = []
+        outputs = []
+        hidden = (h0.view(1, h0.size(0), h0.size(1)), c0.view(1, c0.size(0), c0.size(1)))
+
+        # attention lstm
+        for i in range(t):
+            current_h = h_expand[:, i, :, :, :]
+
+            # (1, n, lstm_hidden) -> (n, lstm_hidden, 1, 1) -> (n, lstm_hidden, 8, 8)
+            previous_lstm_h = hidden[0].view(hidden[0].size(1), hidden[0].size(2), 1, 1).expand((-1, -1, current_h.size(2), current_h.size(3)))
+            # (n, 512+lstm_hidden, 8, 8)
+            attn_input = torch.cat((current_h, previous_lstm_h), dim=1)
+
+            # (n, 512+lstm_hidden, 8, 8) -> (n, 1, 8, 8)
+            attn_map = self.attention(attn_input)
+            attn_map = attn_map.view(n, 1,  8 * 8)
+            attn_map = torch.nn.functional.softmax(attn_map, dim=2)
+            attn_map = attn_map.view(n, 1, 8, 8)
+
+            # (n, 1, 8, 8) * (n, 512, 8, 8) -> (n, 512) -> (n, one time step, 512)
+            attn_applied = torch.sum(attn_map * current_h, dim=(2, 3))
+            attn_applied = attn_applied.view(attn_applied.size(0), 1, attn_applied.size(1))
+
+            output, hidden = self.lstm(attn_applied, hidden)
+
+            attn_maps.append(attn_map)
+            outputs.append(output)
+
+        # (n, t, 1, 8, 8)
+        attn_maps = torch.stack(attn_maps, dim=1)
+        # (n, t, 1, 512)
+        outputs = torch.stack(outputs, dim=1)
+        # take mean of outputs over time 
+        # (n, t, 1, 512) -> (n, 512)
+        outputs = torch.mean(torch.squeeze(outputs), dim=1)
+        # normalize outputs
+        outputs = F.normalize(outputs, p=2, dim=1)
+        # hidden: ((1, n, 512), (1, n, 512))
+        return outputs
+    def get_offset(self):
+            return cebra.data.Offset(5, 5)
+
 def process_brain(brain_seq):
     brain_seq = np.array(brain_seq)
+    # if brain seq > 128 x 128, downsample
+    if brain_seq.shape[1] > 128:
+        # downsample to 128 x 128
+        brain_seq = np.array([cv2.resize(brain_frame, (128, 128)) for brain_frame in brain_seq])
+
     flat_seq = np.array([(brain_frame.flatten()) for brain_frame in brain_seq])
     return flat_seq.astype(float)
 
@@ -129,19 +253,33 @@ def flatten_data(data):
     return np.concatenate(data, axis=0)
 
 def pad_data(data, pre, post):
-    pre_padding = np.repeat(np.expand_dims(data[0], axis = 0), pre, axis=0)
-    post_padding = np.repeat(np.expand_dims(data[-1], axis = 0), post, axis=0)
-    return np.concatenate((pre_padding, data, post_padding), axis=0)
+    print (np.array(data).shape)
+    data = np.array(data)
+    t, x, y = data.shape
+    padded = np.zeros((t + pre + post, x, y))
+    padded[pre:pre + t, :, :] = data
+    padded[:pre, :, :] = data[0:1, :, :]
+    padded[pre + t:, :, :] = data[-1:, :, :]
+    return padded
 
 def generate_CEBRA_embeddings(model, data, session_id, offset = (2,3)):
-    data_torch = torch.empty(0,5,128,128).to('cuda')
-    padded = pad_data(data, offset[0], offset[1])
+    data_torch = torch.empty(0,offset[0] + offset[1],128,128).to('cuda')
+    padded = pad_data(np.squeeze(data), offset[0], offset[1])
+    print (padded.shape)
     for i, frame in enumerate(data):
         frame = torch.from_numpy(np.array(padded[i: i + offset[0] + offset[1]])).float().unsqueeze(0).to('cuda')
         data_torch = torch.cat((data_torch, frame), dim = 0)
     data_torch = data_torch.swapdims(-2, 1)
-    embedding = model.to('cuda')[session_id](data_torch).detach().cpu().numpy().squeeze()
-    return embedding
+    # batch process data to save memory
+    output = None
+    model = model[session_id].eval().to('cuda')
+    for i in range(0, len(data_torch), 100):
+        embedding = model(data_torch[i:i+100]).detach().cpu().numpy().squeeze()
+        if i == 0:
+            output = embedding
+        else:
+            output = np.concatenate((output, embedding), axis = 0)
+    return output
 
 def load_model(model_path):
     #find available device
